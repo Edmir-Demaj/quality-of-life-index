b4c9b5f12fc87cc5fefc811c540233ca851840a1
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-02-07 10:54:03 +0000

    create const for each worksheet to avoid DRY

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+"""
+Import libraries needed for the application to function,
+aswell to style the output for user for better UX.
+"""
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Code below was taken from love_sandwiches walkthrough project
+# with Code Institute. Refered to Credits on README.md file.
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,7 +18,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('quality_of_life_index')
 
-asia = SHEET.worksheet('Asia')
+# Create const variable to open each worksheet
+# so can use later on code easily following the 'DRY' rule.
+AFRICA = SHEET.worksheet('Africa').get_all_values()
+ASIA = SHEET.worksheet('Asia').get_all_values()
+EUROPE = SHEET.worksheet('Europe').get_all_values()
+AMERICA = SHEET.worksheet('America').get_all_values()
+OCEANIA = SHEET.worksheet('Oceania').get_all_values()
 
-data = asia.get_all_values()
-print(data)
+print(EUROPE)
