88d18990d6cb2a2eee7e0a0e3b2dbffe87cda4f6
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-02-10 11:09:10 +0000

    Adjust empty spaces and typing speed for all code

--- run.py
+++ run.py
@@ -84,27 +84,27 @@ def app_info():
     and how is calculated for Countries.
     """
     print(" ")
-    print(ts.W + " Quality of Life Index (higher is better)")
-    print(" is an estimation of overall quality of life,")
-    print(" using an empirical formula which consider:")
+    print(ts.W + "Quality of Life Index (higher is better)")
+    print("is an estimation of overall quality of life,")
+    print("using an empirical formula which consider:")
     time.sleep(3)
-    print(" --------------------------------------------")
-    print(ts.G + " Purchasing power Index (higher is better)\n")
+    print("--------------------------------------------")
+    print(ts.G + "Purchasing power Index (higher is better)\n")
     time.sleep(1)
-    print(ts.Y + " Pollution Index (lower is better)\n")
+    print(ts.Y + "Pollution Index (lower is better)\n")
     time.sleep(1)
-    print(ts.Y + " House price to Income ratio (lower is better)\n")
+    print(ts.Y + "House price to Income ratio (lower is better)\n")
     time.sleep(1)
-    print(ts.Y + " Cost of living Index (lower is better)\n")
+    print(ts.Y + "Cost of living Index (lower is better)\n")
     time.sleep(1)
-    print(ts.G + " Safety Index (higher is better)\n")
+    print(ts.G + "Safety Index (higher is better)\n")
     time.sleep(1)
-    print(ts.G + " Health care Index (higher is better)\n")
+    print(ts.G + "Health care Index (higher is better)\n")
     time.sleep(1)
-    print(ts.Y + " Traffic commute time Index (lower is better)\n")
+    print(ts.Y + "Traffic commute time Index (lower is better)\n")
     time.sleep(1)
-    print(ts.G + " Climate Index (higher is better)")
-    print(ts.W + " --------------------------------------------")
+    print(ts.G + "Climate Index (higher is better)")
+    print(ts.W + "--------------------------------------------")
     time.sleep(6)
     clean_screen()
 
@@ -114,22 +114,24 @@ def get_user_name():
     Get user name from an input method. Run a While Loop
     until input is valid.
     """
+    print(" ")
     print_slowly(ts.W + "Let's start...\n")
     time.sleep(1.5)
     print(" ")
-    print("First insert your name with letters between\n")
+    print("First insert your name with letters between")
     print("2 and 13 characters long and no numbers.\n")
 
     while True:
         username = input(ts.Y + "Please enter your name:\n")
         print(" ")
         print("Validating your input value...\n")
-        time.sleep(1.5)
+        time.sleep(2)
         print(ts.W + "--------------------------------------------")
 
         if validate_username(username):
-            print(ts.G + "Input value entered is valid. Processing...\n")
-            time.sleep(1.5)
+            print(ts.G + "Input value entered is valid. Processing...")
+            print(ts.W + "--------------------------------------------")
+            time.sleep(2)
             clean_screen()
             break
 
@@ -163,7 +165,8 @@ def validate_username(username):
                 f"You entered wrong number of characters: '{len(username)}'"
             )
     except ValueError as error:
-        print(f"{ts.R}Invalid data:\n{error}.\nPlease enter correct value.\n")
+        print(f"{ts.R}Invalid data:\n{error}.\nPlease enter correct value.")
+        print(ts.W + "--------------------------------------------")
         time.sleep(3)
         clean_screen()
         return False
@@ -179,27 +182,29 @@ def select_continent(*name):
     """
     if name:
         user = name[0].capitalize()
+        print(" ")
         print(f"{ts.W}Great {ts.Y}{user} !\n")
         time.sleep(1.5)
-    print(ts.W + "Now please select one of the Continents to get data:\n")
+    print(ts.W + "Now please select one of the Continents to get data:")
 
     while True:
         print(ts.Y + "Type one of the letters [a, b, c, d, e]\n")
-        print(ts.B + "a) Africa\n")
-        print(ts.B + "b) Asia\n")
-        print(ts.B + "c) Europe\n")
-        print(ts.B + "d) America\n")
-        print(ts.B + "e) Oceania\n")
+        print(ts.C + "a) Africa\n")
+        print(ts.C + "b) Asia\n")
+        print(ts.C + "c) Europe\n")
+        print(ts.C + "d) America\n")
+        print(ts.C + "e) Oceania\n")
         choice = input(ts.Y + "Please select one of the options:\n")
         user_choice = choice.lower()
         print(" ")
         print("Validating your input value...\n")
-        time.sleep(1.5)
+        time.sleep(2)
         print(ts.W + "--------------------------------------------")
 
         if validate_cont_choice(user_choice):
-            print(f"{ts.G}Input valid. Proceessing...\n")
-            time.sleep(1.5)
+            print(f"{ts.G}Input valid. Proceessing...")
+            print(ts.W + "--------------------------------------------")
+            time.sleep(2)
             clean_screen()
             break
 
@@ -217,7 +222,8 @@ def validate_cont_choice(user_choice):
         if user_choice not in ["a", "b", "c", "d", "e"]:
             raise ValueError(f"You entered wrong value: '{user_choice}'")
     except ValueError as error:
-        print(f"{ts.R}Invalid selection:\n{error}. Please try again !\n ")
+        print(f"{ts.R}Invalid selection:\n{error}. Please try again ! ")
+        print(ts.W + "--------------------------------------------")
         time.sleep(3)
         clean_screen()
         return False
@@ -262,16 +268,17 @@ def select_country(cont_name):
     until Contry name is valid. Provide data for Country selected.
     """
     user_cont_name = cont_name[1]
-    print(f"{ts.W} Now choose a Country from {ts.Y}{user_cont_name}\n")
-    print(f"{ts.W} to get the {ts.Y}Quality of Life Index.\n")
+    print(" ")
+    print(f"{ts.W}Now choose a Country from {ts.Y}{user_cont_name}")
+    print(f"{ts.W}to get the {ts.Y}Quality of Life Index.\n")
     time.sleep(1)
-    print(f"{ts.R} *** Important ! ***\n")
-    print(f"{ts.W} In order to receive data, the name of the\n")
-    print(f"{ts.W} Country name should be typed as the example:\n")
+    print(f"{ts.R}*** Important ! ***")
+    print(f"{ts.W}In order to receive data, the name of the")
+    print(f"{ts.W}Country name should be typed as the example:\n")
     time.sleep(1)
-    print_slowly(f"{ts.B} Ireland or\n")
-    print_slowly(f"{ts.B} Czech Republic or\n")
-    print_slowly(f"{ts.B} New Zealand etc.\n")
+    print_slowly(f"{ts.C}Ireland or\n")
+    print_slowly(f"{ts.C}Czech Republic or\n")
+    print_slowly(f"{ts.C}New Zealand etc.\n")
     while True:
         print(" ")
         user_country = input(ts.Y + "Please enter the name of Country:\n")
@@ -285,19 +292,22 @@ def select_country(cont_name):
             row = cell.row
             # Get data from the specific row
             country_index = cont_name[0].row_values(row)
-            print(f"Showing data for {country_name}: \n")
-            time.sleep(1)
-            print(f"Quality of Life Index: {country_index[2]}\n")
+            print(" ")
+            print(f"Showing data for {ts.C}{country_name}: \n")
+            time.sleep(1.5)
+            print(f"{ts.W}Quality of Life Index: {country_index[2]}\n")
             print(f"Rank in {cont_name[1]} is: {country_index[0]}\n")
             time.sleep(2)
             print(ts.W + "--------------------------------------------")
             print(ts.G + "If Q.L.I bigger than 160 (High Quality of Life)")
+            time.sleep(1)
             print(" ")
             print(ts.Y + "If Q.L.I between 100-160 (Medium Quality of Life)")
+            time.sleep(1)
             print(" ")
             print(ts.R + "If Q.L.I smaller than 100 (Low Quality of Life)")
             print(ts.W + "--------------------------------------------")
-            time.sleep(5)
+            time.sleep(6)
             clean_screen()
             break
 
@@ -313,18 +323,21 @@ def validate_country(country_name, cont_name):
 
     if country_name in country_column:
         print(" ")
-        print(f"Getting data for {ts.B}{country_name}...\n")
+        print(ts.W + "--------------------------------------------")
+        print(f"{ts.G}Getting data for {ts.C}{country_name}...")
         print(ts.W + "--------------------------------------------")
         time.sleep(1.5)
         clean_screen()
         return True
     else:
+        print(" ")
         print(ts.W + "--------------------------------------------")
-        print(ts.R + "Sorry but we don't have any data for:\n")
-        print(f"{ts.B}'{country_name}' in '{cont_name[1]}'.\n")
-        print(ts.R + "Make sure your Country name is correct!\n")
-        print("Please try again different Country!\n")
-        time.sleep(3)
+        print(ts.R + "Sorry but we don't have any data for:")
+        print(f"{ts.C}'{country_name}' in '{cont_name[1]}'.")
+        print(ts.R + "Make sure your Country name is correct!")
+        print(ts.W + "--------------------------------------------\n")
+        print(ts.Y + "Please try again different Country!")
+        time.sleep(4)
         clean_screen()
         return False
 
@@ -335,6 +348,7 @@ def specific_data(index):
     to get more details about Country selected.
     Validate the answer from user.
     """
+    print(" ")
     print("Would you like to get more specific data?\n")
     answer_choice = input("Enter yes/no:\n")
     answer = answer_choice.lower()
@@ -342,30 +356,31 @@ def specific_data(index):
     if answer == "yes":
         time.sleep(1)
         clean_screen()
-        print(ts.G + "Higher is better 0-100")
-        print(f"Purchasing Power Index is: {index[2][3]}\n")
-        time.sleep(1)
-        print(ts.R + "Lower is better 0-100")
-        print(f"Pollution Index: {index[2][9]}\n")
-        time.sleep(1)
-        print(ts.R + "Lower is better 0-100")
-        print(f"House Price to Income Ratio: {index[2][7]}\n")
-        time.sleep(1)
-        print(ts.R + "Lower is better 0-100")
-        print(f"Cost of Living Index: {index[2][6]}\n")
-        time.sleep(1)
-        print(ts.G + "Higher is better 0-100")
-        print(f"Safety Index is: {index[2][4]}\n")
-        time.sleep(1)
-        print(ts.G + "Higher is better 0-100")
-        print(f"Health Care Index: {index[2][5]}\n")
-        time.sleep(1)
-        print(ts.R + "Lower is better 0-100")
-        print(f"Traffic Commute Time Index: {index[2][8]}\n")
-        time.sleep(1)
-        print(ts.G + "Higher is better 0-100")
-        print(f"Climate Index: {index[2][10]}\n")
-        time.sleep(4)
+        print(f"{ts.W}Showing specific data for {index[0]}: ")
+        print(f"{ts.G}Purchasing Power Index: {index[2][3]}")
+        print("Higher is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.R}Pollution Index: {index[2][9]}")
+        print("Lower is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.R}House Price to Income Ratio: {index[2][7]}")
+        print("Lower is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.R}Cost of Living Index: {index[2][6]}")
+        print("Lower is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.G}Safety Index is: {index[2][4]}")
+        print("Higher is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.G}Health Care Index: {index[2][5]}")
+        print("Higher is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.R}Traffic Commute Time Index: {index[2][8]}")
+        print("Lower is better 0-100\n")
+        time.sleep(1.5)
+        print(f"{ts.G}Climate Index: {index[2][10]}")
+        print("Higher is better 0-100")
+        time.sleep(6)
         clean_screen()
         other_country()
     elif answer == "no":
@@ -373,6 +388,9 @@ def specific_data(index):
         clean_screen()
         other_country()
     else:
+        print(" ")
+        time.sleep(1)
+        print(ts.W + "--------------------------------------------")
         print(ts.R + "Wrong answer! Try again.")
         print(ts.W + "--------------------------------------------")
         time.sleep(2)
@@ -387,6 +405,7 @@ def other_country():
     Ask user if they want data for another country.
     Validate the answer from user.
     """
+    print(" ")
     print(ts.Y + "Would you like to get QLI for another Country?\n")
     other_count = input("Enter yes/no:\n")
     other_count_answer = other_count.lower()
@@ -408,12 +427,17 @@ def other_country():
         print(ts.W + "--------------------------------------------")
         print_slowly(ts.Y + "Thank you for using Quality of Life Index.\n")
         print(" ")
+        print("              **********                   ")
+        print(" ")
         print_slowly("     Copyright Edmir Demaj - 2023\n")
         print(ts.W + "--------------------------------------------")
         print(" ")
-        time.sleep(3)
+        time.sleep(4)
         clean_screen()
     else:
+        print(" ")
+        time.sleep(1)
+        print(ts.W + "--------------------------------------------")
         print(ts.R + "Wrong answer! Try again.")
         print(ts.W + "--------------------------------------------")
         time.sleep(2)
@@ -426,12 +450,12 @@ def main():
     Run all program functions.
     """
     # logo()
-    app_info()
-    # name = get_user_name()
-    # user_cont = select_continent(name)
-    # worksheet = access_sheet(user_cont)
-    # data = select_country(worksheet)
-    # specific_data(data)
+    # app_info()
+    name = get_user_name()
+    user_cont = select_continent(name)
+    worksheet = access_sheet(user_cont)
+    data = select_country(worksheet)
+    specific_data(data)
 
 
 main()
